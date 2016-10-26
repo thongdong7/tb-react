068f39d46e1a14f8d724e58ab6c2508f70dae888
An Vu <an.vu@sentifi.com>
2016-10-26 15:03:30 +0700

    ass switch component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ export * from './mapUtils'
 export * from './form'
 export * from './notify'
 export * from './api'
-import {selectProps} from './props'
+export * from './components'
+export * from './props'
 import store from './simple-redux'
 
 export {
-  selectProps,
-
   store
 }
