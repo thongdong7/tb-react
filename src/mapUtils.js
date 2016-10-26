afb7c8ea68441da86b0fb1fa417078d73ec9bc4b
An Vu <an.vu@sentifi.com>
2016-10-26 15:48:42 +0700

    clean up

--- src/mapUtils.js
+++ src/mapUtils.js
@@ -5,46 +5,6 @@ import {selectProps} from './props'
 
 import * as fs from './form'
 
-export function _get(_api, url, {fields=[], params, defaultData=[]}={}) {
-  let lastAPIParams = null
-  return {
-    init: (props) => {
-      lastAPIParams = null
-      return {...props, data: defaultData}
-    },
-    propsChange: async (nextProps, force) => {
-      let compParams = {}
-      for (const field of fields) {
-        if (nextProps[field]) {
-          compParams[field] = nextProps[field]
-        } else if (nextProps.params && nextProps.params[field]) {
-          // Load from react router params
-          compParams[field] = nextProps.params[field]
-        }
-      }
-
-      const apiParams = {...params, ...compParams}
-//      console.log('api params', apiParams, fields);
-      if (force || !isSameParams(apiParams, lastAPIParams)) {
-//        console.log('load api');
-        let data = await _api.load(url, apiParams)
-
-        // console.log(data);
-        lastAPIParams = apiParams
-
-        return {
-          ...nextProps,
-          data
-        }
-      }
-
-      return nextProps
-    }
-  }
-}
-
-export const get = _get.bind(undefined, api)
-
 export function _buildFormSubmit(_api, url, {fields=[], params}={}) {
   return (props, schema) => async (formData={}) => {
 //        console.log('call', props, form);
@@ -70,62 +30,6 @@ export function _buildFormSubmit(_api, url, {fields=[], params}={}) {
 }
 
 export const buildFormSubmit = _buildFormSubmit.bind(undefined, api)
-
-export function _post(_api, url, {prop, fields=[], params}={}) {
-  return {
-    transfer: (props) => {
-      return {
-        ...props,
-        [prop]: _buildFormSubmit(_api, url, {fields, params})(props)
-      }
-    }
-  }
-}
-
-export const post= _post.bind(undefined, api)
-
-export function _form(_api, formBuilder) {
-  return {
-    transfer: (props) => {
-      const config = formBuilder(props)
-      if (!config) {
-        // No config
-        return props
-      }
-
-      const {
-        url,
-        name="form",
-        fields=[],
-        data={},
-
-        // Form options
-        complete,
-        schema,
-        editable=false
-      } = config
-
-      const formSchema = fs.buildSchema(schema)
-
-      const submit = _buildFormSubmit(_api, url)(props, formSchema)
-
-      return {
-        ...props,
-        [name]: new Form(
-          {...selectProps(props, fields), ...data},
-          submit,
-          {
-            schema,
-            complete,
-            editable
-          }
-        )
-      }
-    }
-  }
-}
-
-export const form = _form.bind(undefined, api)
 
 export function _form2(_api, formBuilder) {
   return {
