97e9197eccde922e9411d87c4edb6139765e77d5
An Vu <an.vu@sentifi.com>
2016-10-27 12:00:53 +0700

    add InputTextForm

---
+++ src/components/InputTextForm.js
@@ -0,0 +1,72 @@
+import React, {Component, PropTypes} from 'react'
+import {APIActionButton} from '../api'
+
+// A simple form with <input text> and Add button
+export class InputTextForm extends Component {
+  constructor(props) {
+    super(props)
+  }
+  state = {
+    value: '',
+    error: false,
+  }
+
+  static propTypes = {
+    action: PropTypes.array.isRequired,
+    onComplete: PropTypes.func
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+
+    this.button.submit()
+  }
+
+  formChange = ({target: {value}}) => {
+    this.setState({value})
+  }
+
+  onComplete = () => {
+    this.setState({error: false, value: ''})
+    if (this.props.onComplete) {
+      this.props.onComplete()
+    }
+  }
+
+  onError = () => {
+    this.setState({error: true})
+  }
+
+  render() {
+    const {action=[]} = this.props
+    const {value, error} = this.state
+
+    const errorClass = error ? 'has-error': ''
+    return (
+      <form
+        role="form"
+        onSubmit={this.onSubmit}
+        onChange={this.formChange}
+      >
+        <div className={`input-group input-group-sm ${errorClass}`}>
+          <input
+            type="text"
+            className={`form-control`}
+            autoComplete="off"
+            value={value}
+            // onKeyPress={this.handleKeyPress}
+          />
+          <span className="input-group-btn">
+            <APIActionButton
+              ref={(r) => this.button = r}
+              action={[...action, value]}
+              name="Add"
+              onError={this.onError}
+              onComplete={this.onComplete}
+            />
+          </span>
+        </div>
+      </form>
+    )
+  }
+}
